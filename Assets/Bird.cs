1199a8ed29e48f5c3715dab09d234689b7d81e17
alexdevs-vg <alexplaysvideogames89@gmail.com>
2021-11-08 15:05:08 +1000

    added music, score, pipe mvmt

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -13,10 +13,17 @@ public class Bird : MonoBehaviour
     public int score;
     private float RotationSpeed = 200.0f;
 
-    public AudioSource gameaudio;
+    public GameObject obstacle1;
+    public GameObject obstacle2;
+    public GameObject obstacle3;
+    bool ob1Reset = true;
+    bool ob2Reset = true;
+    bool ob3Reset = true;
+    public GameObject obstacle12;
+    public GameObject obstacle22;
+    public GameObject obstacle32;
+
     public AudioSource punchSFX;
-    public AudioClip gameAudioClip;
-    public AudioClip punchClip;
 
     // Start is called before the first frame update
     void Start()
@@ -26,9 +33,6 @@ public class Bird : MonoBehaviour
         DeathRotate = false;
         score = 0;
         transform.rotation = Quaternion.Euler(0, 0, 0);
-        
-        gameaudio = GetComponent<AudioSource>();
-        gameaudio.Play();
     }
 
     // Update is called once per frame
@@ -48,11 +52,31 @@ public class Bird : MonoBehaviour
         if (DeathRotate) {
             DeathRotation();
         }
+
+        if ((obstacle1.transform.position[0] < 0) & (ob1Reset) & (DeathRotate==false)) {
+            ob1Reset = false;
+            score = increaseScore(score);
+        } else if (obstacle1.transform.position[0] > 500) {
+            ob1Reset = true;
+        }
+
+        if ((obstacle2.transform.position[0] < 0) & (ob2Reset) & (DeathRotate==false)) {
+            ob2Reset = false;
+            score = increaseScore(score);
+        } else if (obstacle2.transform.position[0] > 500) {
+            ob2Reset = true;
+        }
+
+        if ((obstacle3.transform.position[0] < 0) & (ob3Reset) & (DeathRotate==false)) {
+            ob3Reset = false;
+            score = increaseScore(score);
+        } else if (obstacle3.transform.position[0] > 500) {
+            ob3Reset = true;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("OnCollisionEnter2D");
         if (GameOver==false) {
             GameOver = true;
             rb2D.velocity = new Vector3(0, 200, 0);
@@ -64,10 +88,12 @@ public class Bird : MonoBehaviour
     void resetGame() {
         GameOver = false;
         score = 0;
+        ScoreText.text = score.ToString();
         transform.position = new Vector3(0f,0f,-1.1f);
         transform.rotation = Quaternion.Euler(0, 0, 0);
         rb2D.velocity = new Vector3(0, 0, 0);
         DeathRotate = false;
+        resetPipes();
     }
 
     void DeathRotation() {
@@ -80,5 +106,22 @@ public class Bird : MonoBehaviour
         } else {
             GameOverText.enabled = false;
         }
+    }
+
+    int increaseScore(int score) {
+        score += 1;
+        ScoreText.text = score.ToString();
+        return score;
+    }
+
+    public void resetPipes(){
+        obstacle1.transform.position = new Vector3(250.0f,obstacle1.transform.position[1],obstacle1.transform.position[2]);
+        obstacle12.transform.position = new Vector3(250.0f,obstacle12.transform.position[1],obstacle12.transform.position[2]);
+
+        obstacle2.transform.position = new Vector3(600.0f,obstacle2.transform.position[1],obstacle2.transform.position[2]);
+        obstacle22.transform.position = new Vector3(600.0f,obstacle22.transform.position[1],obstacle22.transform.position[2]);
+
+        obstacle3.transform.position = new Vector3(950.0f,obstacle3.transform.position[1],obstacle3.transform.position[2]);
+        obstacle32.transform.position = new Vector3(950.0f,obstacle32.transform.position[1],obstacle32.transform.position[2]);
     }
 }
--- Assets/PipeScript.cs
+++ Assets/PipeScript.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class PipeScript : MonoBehaviour
 {
+    public GameObject pipe1;
+    public GameObject pipe2;
 
-    public GameObject[] obstacle1;
-    public GameObject[] obstacle2;
-    public GameObject[] obstacle3;
-
-    public float speed = 1f;
+    public float speed = 100f;
     private float obstacleDist = 400f;
+
+    float pipe1Y = -220;
+    float pipe2Y = 170;
+    float shiftamount = 1050f;
+    float RandStrength = 70.0f;
+    public float resetX;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomisePipes(0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        pipe1.transform.Translate(Vector3.left * Time.deltaTime * speed);
+        pipe2.transform.Translate(Vector3.right * Time.deltaTime * speed);
 
         //todo respawn when too far left
-        // todo randomise heights and gap
+        if(pipe1.transform.position[0] < -500) {
+            randomisePipes(shiftamount);
+        }
+
+    }
+
+    void randomisePipes(float shiftamount) {
+        float gap = Random.Range(-RandStrength, RandStrength);
+        float shift = Random.Range(-RandStrength, RandStrength);
+        pipe1.transform.position = new Vector3(pipe1.transform.position[0]+shiftamount,pipe1Y+shift+gap/2,pipe1.transform.position[2]);
+        pipe2.transform.position = new Vector3(pipe2.transform.position[0]+shiftamount,pipe2Y+shift+gap/2,pipe2.transform.position[2]);
     }
 }
